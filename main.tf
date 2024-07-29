6210358301633398682205f0bf739c41ddfc449a
Soumendra Kumar Sahoo <118928639+soumendra2125@users.noreply.github.com>
2024-07-29 16:18:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
 resource "aws_instance" "my_vm" {
  ami           = "ami-04a81a99f5ec58529" //Ubuntu AMI
  instance_type = "t2.micro"
+ key_name      = "laptop-keys"
+ associate_public_ip_address = true
+ security_groups = "sg-09d76213436205016"
+ subnet_id       = "subnet-04f3612e638d29de0"
+ vpc_id   = "vpc-015fbb954ddefaaca"
 
  tags = {
    Name = "My_Instance"
