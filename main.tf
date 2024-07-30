fff23112dc279c1293f1b348f8e5652c51435442
Soumendra Kumar Sahoo <118928639+soumendra2125@users.noreply.github.com>
2024-07-30 12:23:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "my_vm" {
  ami           = "ami-04a81a99f5ec58529" //Ubuntu AMI
  instance_type = "t2.micro"
+ key_name               = "ssh-keys"
  monitoring             = true
  vpc_security_group_ids = ["sg-09d76213436205016"]
  subnet_id              = "subnet-04f3612e638d29de0"
